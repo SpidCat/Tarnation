d8c2556e47d9bebe63b03c7fea10b87ee625d789
SpidCat <148910613+SpidCat@users.noreply.github.com>
2023-10-31 16:14:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,46 +10,101 @@ while True:
   diff = input("Start. Select Difficulty:")
   if diff == '1':
     Number = int(random.randrange(1, 100, 4))
+    Attempts = 25
     break
   if diff == '2':
     Number = int(random.randrange(1, 500, 4))
+    Attempts = 23
     break
   if diff == '3':
     Number = int(random.randrange(1, 1000, 4))
+    Attempts = 21
     break
   if diff == '4':
     Number = int(random.randrange(1, 5000, 4))
+    Attempts = 19
     break
   if diff == '5':
     Number = int(random.randrange(1, 10000, 4))
+    Attempts = 17
     break
   else:
     print("Please select a number 1-5")
-
 while True:
-  Guess = input("Guess a number: ")
-  if int(Guess) < Number:
-     print("Too low")
-  elif int(Guess) > Number:
-     print("Too high")
-  elif int(Guess) == Number:
-    print("You guessed it!")
-    print("1 Easy (1-100)" )
-    print("2 Medium (1-500)")
-    print("3 Hard (1-1000)")
-    print("4 Impossible (????)")
-    print("5 Impossibler (?????)")
-    diff = input("Select The Difficulty Again:")
-    if diff == '1':
-     Number = (random.randrange(1, 100, 4))
-    if diff == '2':
-     Number = (random.randrange(1, 500, 4))
-    if diff == '3':
-     Number = (random.randrange(1, 1000, 4))
-    if diff == '4':
-      Number = (random.randrange(1, 5000, 4))
-    if diff == '5':
-     Number = (random.randrange(1, 10000, 4))
-  else:
+  Guess = input("Guess a number:")
+  try:
+    if int(Guess) < Number:
+     print("Too Low,", Attempts, "Attempts Left")
+     Attempts = int(Attempts) - 1
+     if int(Attempts) < int(1):
+       print("You Lost, Please Try Again")
+       diff = input("Select The Difficulty Again:")
+       if diff == '1':
+         Number = int(random.randrange(1, 100, 4))
+         Attempts = 25
+       if diff == '2':
+         Number = int(random.randrange(1, 500, 4))
+         Attempts = 23
+       if diff == '3':
+         Number = int(random.randrange(1, 1000, 4))
+         Attempts = 21
+       if diff == '4':
+         Number = int(random.randrange(1, 5000, 4))
+         Attempts = 19
+       if diff == '5':
+         Number = int(random.randrange(1, 10000, 4))
+         Attempts = 17
+       else:
+        print("Please select a number 1-5")
+    elif int(Guess) > Number:
+     print("Too high,", Attempts, "Attempts Left")
+     Attempts = int(Attempts) - 1
+     if int(Attempts) < int(1):
+       print("You Lost, Please Try Again")
+       diff = input("Select The Difficulty Again:")
+       if diff == '1':
+         Number = int(random.randrange(1, 100, 4))
+         Attempts = 25
+       if diff == '2':
+         Number = int(random.randrange(1, 500, 4))
+         Attempts = 23
+       if diff == '3':
+         Number = int(random.randrange(1, 1000, 4))
+         Attempts = 21
+       if diff == '4':
+         Number = int(random.randrange(1, 5000, 4))
+         Attempts = 19
+       if diff == '5':
+         Number = int(random.randrange(1, 10000, 4))
+         Attempts = 17
+       else:
+        print("Please select a number 1-5")
+    elif int(Guess) == Number:
+      print("You guessed it!")
+      print("1 Easy (1-100)" )
+      print("2 Medium (1-500)")
+      print("3 Hard (1-1000)")
+      print("4 Impossible (????)")
+      print("5 Impossibler (?????)")
+      diff = input("Select The Difficulty Again:")
+      if diff == '1':
+       Number = int(random.randrange(1, 100, 4))
+       Attempts = 25
+      if diff == '2':
+       Number = int(random.randrange(1, 500, 4))
+       Attempts = 23
+      if diff == '3':
+       Number = int(random.randrange(1, 1000, 4))
+       Attempts = 21
+      if diff == '4':
+       Number = int(random.randrange(1, 5000, 4))
+       Attempts = 19
+      if diff == '5':
+       Number = int(random.randrange(1, 10000, 4))
+       Attempts = 17
+      else:
+       print("Please select a number 1-5")
+
+  except ValueError:
    print("Invalid Input")
    continue
