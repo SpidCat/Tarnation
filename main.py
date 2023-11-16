18bac13d992fbdd8f2bfdabed7eabff4eb70e270
SpidCat <148910613+SpidCat@users.noreply.github.com>
2023-11-16 16:11:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,32 @@ print("3 Hard (1-1000)")
 print("4 Impossible (????)")
 print("5 Impossibler (?????)")
 # Stating Difficulties
-
+def difficulty():
+      print("You guessed it!")
+      print("1 Easy (1-100)" )
+      print("2 Medium (1-500)")
+      print("3 Hard (1-1000)")
+      print("4 Impossible (????)")
+      print("5 Impossibler (?????)")
+      diff = input("Select The Difficulty Again:")
+        if diff == '1':
+         Number = int(random.randrange(1, 100, 4))
+         Attempts = 25
+        if diff == '2':
+         Number = int(random.randrange(1, 500, 4))
+         Attempts = 23
+        if diff == '3':
+         Number = int(random.randrange(1, 1000, 4))
+         Attempts = 21
+        if diff == '4':
+         Number = int(random.randrange(1, 5000, 4))
+         Attempts = 19
+        if diff == '5':
+         Number = int(random.randrange(1, 10000, 4))
+         Attempts = 17
 diff = 0
 Number = 0
+myFavoriteNumbers = ["420","31","97"]
 
 while True:
   diff = input("Start. Select Difficulty:")
@@ -96,28 +119,7 @@ while True:
         print("Please select a number 1-5")
     elif int(Guess) == Number:
   # In Case Of Correct Guess
-      print("You guessed it!")
-      print("1 Easy (1-100)" )
-      print("2 Medium (1-500)")
-      print("3 Hard (1-1000)")
-      print("4 Impossible (????)")
-      print("5 Impossibler (?????)")
-      diff = input("Select The Difficulty Again:")
-      if diff == '1':
-       Number = int(random.randrange(1, 100, 4))
-       Attempts = 25
-      if diff == '2':
-       Number = int(random.randrange(1, 500, 4))
-       Attempts = 23
-      if diff == '3':
-       Number = int(random.randrange(1, 1000, 4))
-       Attempts = 21
-      if diff == '4':
-       Number = int(random.randrange(1, 5000, 4))
-       Attempts = 19
-      if diff == '5':
-       Number = int(random.randrange(1, 10000, 4))
-       Attempts = 17
+      difficulty()
       else:
        print("Please select a number 1-5")
 
